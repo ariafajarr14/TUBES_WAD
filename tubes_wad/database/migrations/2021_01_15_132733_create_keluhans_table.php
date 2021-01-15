b6a1f104b7b4209d628f3b4d4e6fcd490e577bf5
yusufheriantomanik <yusufheri.telkom.mdn@gmail.com>
2021-01-15 20:40:51 +0700

    buat model keluhan

---
+++ tubes_wad/database/migrations/2021_01_15_132733_create_keluhans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKeluhansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keluhans', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->text('detail');
+            $table->string('durasi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keluhans');
+    }
+}
